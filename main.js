e52c0fc82ad59cb5fbcceeb54b7888b93f50fbec
Poós Dániel <poosdani@gmail.com>
2021-10-05 11:37:59 -0400

    egy restart megoldotta a problemam

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
 
-let filmTomb = [];
+var filmTomb = [];
 
 class Film {
+    nev;
+    rendezo;
+    kiadasiEv;
     constructor(nev, rendezo, kiadasiEv){
         this.nev = nev;
         this.rendezo = rendezo;
         this.kiadasiEv = kiadasiEv;
     }
-    string(){
-        return 'Cim: '+this.nev+', rendezte: '+this.rendezo+', kiadas eve: '+this.kiadasiEv;
+    get toString(){
+        return "Cim: "+this.nev+", rendezte: "+this.rendezo+", kiadas eve: "+this.kiadasiEv;
     }
 }
-
-function hozzaAd(nev, re, ki) {
-    nev = document.getElementsByName('nev').value;
-    re = document.getElementsByName('rendezo').value;
-    ki = document.getElementsByName('kiadas').value;
-    filmTomb.push(new Film(nev,re,ki));
-    document.getElementsByName('nev').value = '';
-    document.getElementsByName('rendezo').value= '';
-    document.getElementsByName('kiadas').value= '0';
+function hozzaad() {
+    let nev = document.getElementById('nev').value;
+    let re = document.getElementById('rendezo').value;
+    let ki = document.getElementById('kiadas').value;
+    let adat = new Film(nev,re,ki);
+    filmTomb.push(adat); 
+    document.getElementById('nev').value = '';
+    document.getElementById('rendezo').value= '';
+    document.getElementById('kiadas').value= '';
+}
+function listaz(){
+    let tablazat = "<table><tr><th>Film neve</th><th>Rendezo neve</th><th>Kiadas eve</th></tr>"
+    for (let index = 0; index < filmTomb.length; index++) {
+        tablazat += "<tr><td>"+filmTomb[index].nev+"</td><td>"+filmTomb[index].rendezo+"</td><td>"+filmTomb[index].kiadasiEv+"</td></tr>";
+    }
+    tablazat+="</table>";
+    document.getElementById("tablazat").innerHTML = tablazat;
+    /*for (let index = 0; index < filmTomb.length; index++) {
+        let tr = document.createElement("tr");
+        let th1 = document.createElement("th");
+        let th2 = document.createElement("th");
+        let th3 = document.createElement("th");
+        let tablazat = document.getElementById("tablazat");
+        tr.appendChild(th1.appendChild(document.createTextNode("Film neve")));
+        tr.appendChild(th2.appendChild(document.createTextNode("Rendezo neve")));
+        tr.appendChild(th3.appendChild(document.createTextNode("Kiadas eve")));
+        tablazat.appendChild(tr);
+    }*/
 }
