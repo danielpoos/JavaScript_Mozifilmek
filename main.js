3a25ccfebcdf4314f0ece64f58d7bc668e9bed5a
Poós Dániel <poosdani@gmail.com>
2021-10-05 12:47:30 -0400

    funkciok egyesitese, oldal formazasa

--- main.js
+++ main.js
@@ -18,20 +18,25 @@ function hozzaad() {
     let nev = document.getElementById('nev').value;
     let re = document.getElementById('rendezo').value;
     let ki = document.getElementById('kiadas').value;
+    if(nev == '' || re ==''||ki == ''){
+        alert("Minden mezot toltson ki!")
+    }
+    else{
     let adat = new Film(nev,re,ki);
     filmTomb.push(adat); 
     document.getElementById('nev').value = '';
     document.getElementById('rendezo').value= '';
     document.getElementById('kiadas').value= '';
-}
-function listaz(){
     let tablazat = "<table><tr><th>Film neve</th><th>Rendezo neve</th><th>Kiadas eve</th></tr>"
     for (let index = 0; index < filmTomb.length; index++) {
         tablazat += "<tr><td>"+filmTomb[index].nev+"</td><td>"+filmTomb[index].rendezo+"</td><td>"+filmTomb[index].kiadasiEv+"</td></tr>";
     }
     tablazat+="</table>";
     document.getElementById("tablazat").innerHTML = tablazat;
-    /*for (let index = 0; index < filmTomb.length; index++) {
+    }
+}
+
+/*for (let index = 0; index < filmTomb.length; index++) {
         let tr = document.createElement("tr");
         let th1 = document.createElement("th");
         let th2 = document.createElement("th");
@@ -42,4 +47,3 @@ function listaz(){
         tr.appendChild(th3.appendChild(document.createTextNode("Kiadas eve")));
         tablazat.appendChild(tr);
     }*/
-}
